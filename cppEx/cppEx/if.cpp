65246033f6ac5cff971c448c1fe2466c9d08f9d8
leesunku <tjsrn0524@naver.com>
2017-02-09 23:52:04 +0900

    if

---
+++ cppEx/cppEx/if.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include <iostream> 
+using namespace std;
+
+int main() {
+	int age;
+	age = 20;
+	if (age <= 19) {
+		cout << "미성년자\n";
+	}
+	else {
+		cout << "성인\n";
+	}
+}
